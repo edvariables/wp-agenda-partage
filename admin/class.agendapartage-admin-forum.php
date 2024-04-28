f693de57bb33ff9c0a80156ceb42c341bfd4f21e
edvariables <edid@free.fr>
2024-04-28 15:04:15 +0200

    Column Forum

--- admin/class.agendapartage-admin-forum.php
+++ admin/class.agendapartage-admin-forum.php
@@ -20,7 +20,11 @@ class AgendaPartage_Admin_Forum {
 		//add custom columns for list view
 		add_filter( 'manage_' . AgendaPartage_Forum::post_type . '_posts_columns', array( __CLASS__, 'manage_columns' ) );
 		add_action( 'manage_' . AgendaPartage_Forum::post_type . '_posts_custom_column', array( __CLASS__, 'manage_custom_columns' ), 10, 2 );
-		
+		add_filter( 'manage_edit-' . AgendaPartage_Forum::post_type . '_sortable_columns', array( __CLASS__, 'manage_sortable_columns' ) );
+		if(basename($_SERVER['PHP_SELF']) === 'edit.php'
+		&& isset($_GET['post_type']) && $_GET['post_type'] === AgendaPartage_Forum::post_type)
+			add_action( 'pre_get_posts', array( __CLASS__, 'on_pre_get_posts'), 10, 1);
+
 	}
 	/**
 	 * Pots list view
@@ -57,6 +61,26 @@ class AgendaPartage_Admin_Forum {
 					echo sprintf('<code>%s</code>', $emails);
 				break;
 			default:
+				break;
+		}
+	}
+	public static function manage_sortable_columns( $columns ) {
+		$columns[AgendaPartage_Forum::tag] = AgendaPartage_Forum::tag;
+		return $columns;
+	}
+	/**
+	 * Sort custom column
+	 */
+	public static function on_pre_get_posts( $query ) {
+		global $wpdb;
+		if(empty($query->query_vars)
+		|| empty($query->query_vars['orderby']))
+			return;
+		debug_log('on_pre_get_posts', $query->query_vars['orderby']);
+		switch( $query->query_vars['orderby']) {
+			case AgendaPartage_Forum::tag:
+				$query->set('meta_key', AGDP_PAGE_META_MAILBOX);  
+				$query->set('orderby','meta_value'); 
 				break;
 		}
 	}
