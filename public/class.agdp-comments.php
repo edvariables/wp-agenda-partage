e161535fc7e088a1a2b5db1e33220afd130a646f
edvariables <edid@free.fr>
2026-03-29 13:48:18 +0200

    Update class.agdp-comments.php

--- public/class.agdp-comments.php
+++ public/class.agdp-comments.php
@@ -751,7 +751,7 @@ class Agdp_Comments {
 
 				$html .= sprintf(
 					'<td class="comment-edit"><a href="%s">'
-						.'Afficher le message (et vérifier qu\'il est toujours d\'actualité)'
+						.'Afficher le message'
 						. ($email_mode  ? '' : Agdp::icon('media-default'))
 						.'</a>'
 					.'</td>'
